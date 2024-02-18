f7877a1e855f3d68627a56a27f8b702195541428
Kingbah001 <abdhakeembukhari@gmail.com>
2024-02-18 10:42:50 +0100

    good and impressive

---
+++ style.js
@@ -0,0 +1,8 @@
+
+var hamburgerdiv = document.querySelector(".hamburger");
+var mobileLinks = document.querySelector(".mobile-controlls")
+
+function dosomething (){
+    hamburgerdiv.classList.toggle("showburger")
+    mobileLinks.classList.toggle("show-mobile-controlls")
+} 
